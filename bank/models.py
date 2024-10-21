9217543231094aa3bb203e8540315f05f39c7109
Merey <mursalbekov.merey@gmail.com>
2024-10-21 22:20:03 +0500

    fix models

--- bank/models.py
+++ bank/models.py
@@ -41,4 +41,4 @@ class Transaction(models.Model):
     description = models.TextField(blank=True)
 
     def __str__(self):
-        return f"Transaction of {self.amount} from {self.sender} to {self.receiver} on {self.timestamp}"
+        return f"Transaction of {self.amount:.2f} from {self.sender} to {self.receiver} on {self.timestamp}"
